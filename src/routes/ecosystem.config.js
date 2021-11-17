554047b1d5147ef477ca3b06e86643eae6840f20
Josh <joshbenaron@gmail.com>
2021-11-17 10:17:35 +0000

    Add ecosystem file

---
+++ src/routes/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports =     // ecosystem.js
+{
+  "apps": [
+    {
+      "name": "HttpServer",
+      "script": "./target/release/bundler-balancer",// name of the startup file
+      "exec_mode": "fork",  // to turn on cluster mode; defaults to 'fork' mode
+      "env": {
+        "PORT": "10000", // the port on which the app should listen
+        "kill_timeout": 120000
+      }
+    }
+    ]
+};
