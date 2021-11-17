69aa9101e6eafa217d84a68629df4143ff7fe748
Josh <joshbenaron@gmail.com>
2021-11-17 15:18:44 +0000

    Update port to 80 in ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports =     // ecosystem.js
       "exec": "none",
       "exec_mode": "fork",  // to turn on cluster mode; defaults to 'fork' mode
       "env": {
-        "PORT": "10000", // the port on which the app should listen
+        "PORT": "80", // the port on which the app should listen
         "kill_timeout": 120000
       }
     }
