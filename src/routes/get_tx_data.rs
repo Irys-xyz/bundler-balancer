3f687ec3becc517835b5a0ce5f2ba0a3ebb66c30
joshbenaron <73971531+joshbenaron@users.noreply.github.com>
2023-03-06 15:23:15 +0000

    :sparkles: Add support for GET /tx

--- src/routes/get_tx_data.rs
+++ src/routes/get_tx_data.rs
@@ -49,3 +49,52 @@ pub async fn get_tx_data(
     .insert_header(("Cache-Control", "max-age=0"))
     .finish())
 }
+
+pub async fn get_tx_meta(
+    bundlers: Data<Vec<String>>,
+    client: Data<awc::Client>,
+    path: Path<(String,)>
+) -> actix_web::Result<HttpResponse> {
+    let (tx_id,) = path.into_inner();
+    for bundler in bundlers.iter() {
+        let url = format!("{}/tx/{}", bundler, tx_id);
+        // Create request builder, configure request and send
+        let request = client
+            .head(&url)
+            .send()
+            .await;
+
+        match request {
+            Ok(req) => {
+                if req.status().is_success() {
+                    info!("Found {} at {}", tx_id, bundler);
+                    debug!("Headers {:?}", req.headers());
+                    if let Some(h) = req.headers().get("Content-Length") {
+                        return Ok(HttpResponse::Found()
+                            .insert_header(("Content-Length", h))
+                            .insert_header(("Location", url))
+                            .insert_header(("Cache-Control", "max-age=86400"))
+                            .finish());
+                    } else {
+                        return Ok(HttpResponse::Found()
+                            .insert_header(("Location", url))
+                            .insert_header(("Cache-Control", "max-age=86400"))
+                            .finish());
+                    }
+                } else {
+                    info!("Not found {} at {}", tx_id, bundler);
+                    continue;
+                }
+            },
+            Err(e) => {
+                info!("Error occurred while getting {} from {} - {}", tx_id, bundler, e);
+                continue;
+            }
+        }
+    }
+    
+
+    Ok(HttpResponse::NotFound()
+    .insert_header(("Cache-Control", "max-age=0"))
+    .finish())
+}
